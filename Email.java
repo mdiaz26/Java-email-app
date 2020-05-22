edf6d2b1fc434ba7bc1ae0d396613ff296ebfb01
mdiaz26 <mike.diaz006@gmail.com>
2020-05-22 17:03:47 -0400

    initial commit

---
+++ Email.java
@@ -0,0 +1,24 @@
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    // Constructor to receive the firstName and lastName
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+    }
+    // Ask for the department
+
+    // Generate a random password
+
+    // Set the mailbox capcity
+
+    // Set the alternateEmail
+
+    // Change the password
+}
